bbd7b6611d25939db31ef4d2db6708697091dd58
Kristine Brown <kristinebrown10@gmail.com>
2014-07-17 14:24:30 -0700

    Test shouldThrowIllegalArgumentExceptionWhenNumberIsNegative passed. (5/5!)

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException{
+        if (i < 0)
+            throw new IllegalArgumentException();
         if (i == 1 || i == 0)
             return 1;
         else if (i == 2)
