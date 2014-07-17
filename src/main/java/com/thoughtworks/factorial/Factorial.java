e43d173b02de2c9eb39d7702cd9cb38d48a0d1df
Kristine Brown <kristinebrown10@gmail.com>
2014-07-17 14:27:31 -0700

    All tests passed.  Cleaning up compute method.

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -4,11 +4,9 @@ public class Factorial {
     public Integer compute(int i) throws IllegalArgumentException{
         if (i < 0)
             throw new IllegalArgumentException();
-        if (i == 1 || i == 0)
+        else if (i < 2)
             return 1;
-        else if (i == 2)
-            return 2;
-        else if (i > 2)
+        else if (i >= 2)
             return (i*compute(i-1));
         return -1;
     }
